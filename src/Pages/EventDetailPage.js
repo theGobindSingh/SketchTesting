8d83dabac664b005bc0baa2c910af5750d7d7628
theGobindSingh <android.gobind@gmail.com>
2021-09-26 22:42:01 +0530

    Sketch

--- src/Pages/EventDetailPage.js
+++ src/Pages/EventDetailPage.js
@@ -2,14 +2,87 @@ import "./EventDetailPage.css";
 import React from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import Moment from "react-moment";
+import { useQuery, gql } from "@apollo/client";
+import Loading from "../Components/Loading";
+import Error from "../Components/Error";
 
+const eve = gql`
+  query ($id: ID!) {
+    event(id: $id) {
+      Poster {
+        url
+      }
+      Title
+      EventDate
+      Content
+    }
+  }
+`;
 export default function EventDetailPage() {
   const { id } = useParams();
 
+  const { loading, error, data } = useQuery(eve, { variables: { id: id } });
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
   return (
     <div id="EventDetailDIV">
-      <div className="eveImgCont" style={{ display: "flex" }}>
-        Wait pandra
+      <div
+        className="Eve title container"
+        style={{
+          marginBottom: "15px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <h1>{data.event.Title}</h1>
+      </div>
+      <div
+        className="Eve img container"
+        style={{
+          marginBottom: "15px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src={data.event.Poster.url}
+          alt={data.event.Title}
+          style={{
+            marginBottom: "15px",
+            borderRadius: "50%",
+            maxWidth: "20%",
+            height: "auto",
+            objectFit: "contain",
+          }}
+        ></img>
+      </div>
+      <div
+        className="Eve date container"
+        style={{
+          marginBottom: "15px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <h3>
+          <Moment format="DD MMM YYYY" children={data.event.EventDate} />
+        </h3>
+      </div>
+      <div
+        className="Eve content container"
+        style={{
+          borderRadius: "10px",
+          padding: "4%",
+          paddingLeft: "5%",
+          background: "rgba(215, 215, 255,0.2)",
+        }}
+      >
+        <ReactMarkdown>{data.event.Content}</ReactMarkdown>
       </div>
     </div>
   );
